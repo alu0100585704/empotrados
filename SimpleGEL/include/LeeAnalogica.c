6d6176455fedbe1f69dfae52bb00d0daa46c987f
alu0100958379 <56160161+alu0100958379@users.noreply.github.com>
2020-11-30 18:40:35 +0000

    Update LeeAnalogica.c
    Añadidos comentarios y justificación

--- SimpleGEL/include/LeeAnalogica.c
+++ SimpleGEL/include/LeeAnalogica.c
@@ -22,187 +22,211 @@
 
 //presentar proyecto ad.c ad.h test_ad_xxxx.c  makefile  ... 
 
-uint16_t DirAD_; ///desplazamiento modulo 0 o 1
-uint8_t Pin_; /*Pin dentro del puerto del que se va a leer */
-uint8_t Resolucion_; ///ocho es cero y 10 bits si es uno
-uint8_t ciclosMuestreo_; ///ciclos de muestreo
+uint16_t DirAD_; 		/** Desplazamiento de la dirección para escoger módulo 0 o 1 */
+uint8_t Pin_; 			/** Pin dentro del puerto del que se va a leer */
+uint8_t Resolucion_; 		/** Resolución de los datos (8 o 10 bits) */
+uint8_t ciclosMuestreo_; 	/** Ciclos de muestreo */
 
-
+/**
+ * @brief	Función para seleccionar el valor de los ciclos de muestreo (2,4,8,16)
+ * @param	valor		valor que se define por defecto, en caso de que el usuario no desee modificar nada
+ * @param	interactivo	está a 1 si el usuario quiere interactuar
+ */
 ad_ciclos_muestreo(uint8_t valor, uint8_t interactivo)
 {
 	char c;
 	if (interactivo)
 	{ 
-	 serial_print("\nCiclos de Muestreo 2,4,8,16 (0=2, 1=4, 2=8, 3=16)?:");
-     while((c = serial_recv()) < '0' && c > '3');
-    serial_send(c); /* a modo de confirmación*/     
-     ciclosMuestreo_ = c - '0';    
+		serial_print("\nCiclos de Muestreo de 2, 4, 8 o 16 (0 = 2, 1 = 4, 2 = 8 y 3 = 16):");
+     		while((c = serial_recv()) < '0' && c > '3');
+    		serial_send(c); /* a modo de confirmación*/     
+     		ciclosMuestreo_ = c - '0';    
 	}
 	else
 		ciclosMuestreo_ = valor;	
 }
 
+/**
+ * @brief	Función para seleccionar el valor de la conversión (8 o 10 bits)
+ * @param	valor		valor que se define por defecto, en caso de que el usuario no desee modificar nada
+ * @param	interactivo	está a 1 si el usuario quiere interactuar
+ */
 ad_ocho_o_diez_bits(uint8_t valor, uint8_t interactivo)
 {
 	char c;
 	if (interactivo)
 	{ 
-	 serial_print("\nResolución 8 o 10 Bits (0=8 bits | 1 = 10 bits)?:");
-     while((c = serial_recv()) < '0' && c > '1');
-    serial_send(c); /* a modo de confirmación*/
-     Resolucion_ = c - '0';    
+		serial_print("\nResolución de 8 o 10 Bits (0 para 8 bits o 1 para 10 bits):");
+     		while((c = serial_recv()) < '0' && c > '1');
+    		serial_send(c); /* a modo de confirmación*/
+     		Resolucion_ = c - '0';    
 	}
 	else
 		Resolucion_ = valor;	
-	
-
 }
+
+/**
+ * @brief	Función para seleccionar el tiempo de muestreo
+ * @param	valor		valor que definirá el usuario para el modo
+ * @param	interactivo	está a 1 si el usuario quiere interactuar
+ */
 void ad_tiempo_muestreo(uint16_t valor)
 {
+	
 }
-///
-///
-///
+
+/**
+ * @brief	Función para seleccionar el pin de inicio en el microcontrolador
+ * @param	valor		valor que se define por defecto, en caso de que el usuario no desee modificar nada 
+ * @param	interactivo	está a 1 si el usuario quiere interactuar
+ */
 void ad_pin_inicio(uint8_t valor,uint8_t interactivo)
 {
 	char c;
 	if (interactivo)
-	{ 
-	 serial_print("\nPin del puerto a utilizar (0 - 7)?:");
-     while((c = serial_recv()) < '0' && c > '7');
-    serial_send(c); /* a modo de confirmación*/
-    Pin_ = c - '0'; 
+	{
+		serial_print("\nPin del puerto a utilizar (0 - 7)?:");
+     		while((c = serial_recv()) < '0' && c > '7');
+    		serial_send(c); /** a modo de confirmación */
+    		Pin_ = c - '0'; 
 	}
 	else
-		Pin_=valor;	
+		Pin_ = valor;	
 }
+
+/**
+ * @brief	Función para seleccionar el módulo correspondiente al conversor A/D
+ * @param	valor		valor que se define por defecto, en caso de que el usuario no desee modificar nada 
+ * @param	interactivo	está a 1 si el usuario quiere interactuar
+ */
 void ad_modulo(uint8_t valor, uint8_t interactivo)
 {
 	char c;
 	if (interactivo)
 	{
-		serial_print("\nPuerto conversor a utilizar");
+		serial_print("\nMódulo conversor a utilizar");
 		while((c = serial_recv()) != '0' && c != '1');
 		serial_send(c); /* a modo de confirmación*/
 		DirAD_ = (c == '0')?0:(M6812_ATD1CTL0 - M6812_ATD0CTL0);
 	}
-	 else
-	    if (valor)
-				DirAD_ = (M6812_ATD1CTL0 - M6812_ATD0CTL0);
+	else
+	{
+		if (valor)
+			DirAD_ = (M6812_ATD1CTL0 - M6812_ATD0CTL0);
 		else 
-				DirAD_ = 0;
+			DirAD_ = 0;
+	}
 }
-///
-/// Iniciar conversor
-///
-void ad_iniciar(void)
+
+/**
+ * @brief	Función para completar los parámetros y dar por iniciada la conversión.
+ *		En ella se hace uso de los atributos globales para usarlos como máscara
+ *		para completar los registros correctamente.
+ */
+void ad_iniciar ()
 {
-	  ///* resolución de 10 bits u ocho bits
+	/** Se mira el tipo de resolución */
 	if (Resolucion_)			
 		_io_ports[M6812_ATD0CTL4 + DirAD_] |= M6812B_RES10 ;
-    else
+    	else
 		_io_ports[M6812_ATD0CTL4 + DirAD_] &= ~M6812B_RES10;	
-		  
-	///pin inicial
-	 _io_ports[M6812_ATD0CTL5 + DirAD_] |=  Pin;
+
+	/** Se añade el pin que se va a utilizar */
+	 _io_ports[M6812_ATD0CTL5 + DirAD_] |=  Pin_;
 	
-	///Definimos los cilos de   muestreo
-	 
-	 switch (ciclosMuestreo_)
-	 {
-		 case 0: 
+	/** Dependiendo del valor seleccionado, se activara unos bits u otros para el valor de ciclos de muestreo (SMP0 Y SMP1) */
+	switch (ciclosMuestreo_)
+	{
+		case 0: 
 			_io_ports[M6812_ATD0CTL4 + DirAD_] &= (~M6812B_SMP0 & ~M6812B_SMP1);
-			  break;
-	     case 1:
-	     _io_ports[M6812_ATD0CTL4 + DirAD_] &=  (M6812B_SMP0 | ~M6812B_SMP1); 
-								
 			break;
-		 case 2:
+	     	case 1:
+	     		_io_ports[M6812_ATD0CTL4 + DirAD_] &=  (M6812B_SMP0 | ~M6812B_SMP1); 					
+			break;
+		case 2:
 			_io_ports[M6812_ATD0CTL4 + DirAD_] &=  (~M6812B_SMP0 | M6812B_SMP1); 
 			break;
-		 case 3:
-		 _io_ports[M6812_ATD0CTL4 + DirAD_] |=  (M6812B_SMP0 | M6812B_SMP1); 
+		case 3:
+			_io_ports[M6812_ATD0CTL4 + DirAD_] |=  (M6812B_SMP0 | M6812B_SMP1); 
 			break;
-		 
-	 }
-	 
-	 ///Encendido y justificacion a izquierda
-	  _io_ports[M6812_ATD0CTL2 + DirAD_] = M6812B_ADPU; /*Encendemos, justf. izda*/
+		default:
+			break;
+	}
+
+	/** Se enciende el módulo correspondiente y se justifican los datos a la izquierda */
+	_io_ports[M6812_ATD0CTL2 + DirAD_] = M6812B_ADPU;
 	  	  
-	  ///Sin fifo
-	  _io_ports[M6812_ATD0CTL3 + DirAD_] &= ~M6812B_FIFO; /*Sin fifo*/
-    
-
+	/** Se desactiva FIFO */
+	_io_ports[M6812_ATD0CTL3 + DirAD_] &= ~M6812B_FIFO;
+  
 }
-
 
 void ad_esperar_terminar()
 {
-}
-void ad_devolver_valore_leidos()
-{
+	
 }
 
-int main() {
+void ad_devolver_valore_leidos()
+{
+	
+}
+
+int main () {
+	
 	char c;
 	uint16_t DirAd;
 	uint8_t Pin;
-
-
-    ad_pin_inicio(0,1); ///indicamos pin de entrada
+	
+    	ad_pin_inicio(0,1); 
     
-	ad_modulo(0,1); ///preguntamos por módulo
+	ad_modulo(0,1);
 	
 	ad_ocho_o_diez_bits(0,1); ///resio_ports[M6812_ATD0CTL5 + DirAD] = M6812B_SCAN | M6812B_S8C | Pin;olución solicitada
    
-    ad_ciclos_muestreo(0,1);
+    	ad_ciclos_muestreo(0,1);
     
-    ad_iniciar();
+    	ad_iniciar();
     
-    
-    
-     serial_print("\nConvirtiendo (pulsa para salir)\n");
+	//CONVERSIÓN
+	serial_print("\nConvirtiendo (pulsa para salir)\n");
 
-    char simbolo[] = "/|\\-*";
-    uint16_t itera = 0;
-#define ITERA_CAMBIO (5000)
-    while(!serial_receive_pending()) {
+    	char simbolo[] = "/|\\-*";
+    	uint16_t itera = 0;
+	#define ITERA_CAMBIO (5000)
+    	while(!serial_receive_pending()) {
+		itera++;
+      		if (!(itera % ITERA_CAMBIO)) {
+			uint8_t simAct = itera/ITERA_CAMBIO;
+			if(!simbolo[simAct]) {
+				itera = 0;
+				simAct = 0;
+			}
+			serial_send(simbolo[simAct]);
+			serial_send('\b');
+		}
 
-      itera++;
-      if (!(itera % ITERA_CAMBIO)) {
-        uint8_t simAct = itera/ITERA_CAMBIO;
-        if(!simbolo[simAct]) {
-          itera = 0;
-          simAct = 0;
-        }
-        serial_send(simbolo[simAct]);
-        serial_send('\b');
-      }
+		/* Esperamos a que se termine la conversión */
+		while(! (_io_ports[M6812_ATD0STAT0 + DirAD_] & M6812B_SCF));
 
-      /* Esperamos a que se termine la conversión */
-      while(! (_io_ports[M6812_ATD0STAT0 + DirAD_] & M6812B_SCF));
+		/*Invertimos el led*/
+		_io_ports[M6812_PORTG] ^= M6812B_PG7;
 
-      /*Invertimos el led*/
-      _io_ports[M6812_PORTG] ^= M6812B_PG7;
+		/*Vemos si los 8 resultados son iguales */
+		uint16_t resultado = _IO_PORTS_W(M6812_ADR00H + DirAD_);
+		uint8_t iguales = 1;
+		for(uint8_t i = 0; iguales && i < 8; i++)
+			iguales = resultado == _IO_PORTS_W(M6812_ADR00H + DirAD_ + 2 * i);
+		if(! iguales)
+			continue;
+		if (resultado == resultadoAnterior)
+			continue;
 
-      /*Vemos si los 8 resultados son iguales */
-      uint16_t resultado = _IO_PORTS_W(M6812_ADR00H + DirAD_);
-      uint8_t iguales = 1;
-      for(uint8_t i = 0; iguales && i < 8; i++)
-        iguales = resultado == _IO_PORTS_W(M6812_ADR00H + DirAD_ + 2 * i);
-      if(! iguales)
-        continue;
-      if (resultado == resultadoAnterior)
-        continue;
-
-      /* Los 8 resultados son iguales y distintos a lo que teníamos antes*/
-      serial_print("Nuevo valor = ");
-      serial_printdecword(resultado);
-      serial_send('\n');
-      resultadoAnterior = resultado;
-    }
-    
-    
+		/* Los 8 resultados son iguales y distintos a lo que teníamos antes*/
+		serial_print("Nuevo valor = ");
+		serial_printdecword(resultado);
+		serial_send('\n');
+		resultadoAnterior = resultado;
+	}
 }
 
 ///int main () {
