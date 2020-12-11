052c85a264cf88c4110cce6b3b522f273256ad06
juan <alu0100585704@ull.edu.es>
2020-12-11 10:02:01 +0100

    agregada parte de conversor analógico digital

--- SimpleGEL/7-segmentos/prueba_sieteseg.c
+++ SimpleGEL/7-segmentos/prueba_sieteseg.c
@@ -1,17 +1,24 @@
 #include <stdio.h>
 #include <timer.h>
 #include <gpio.h>
+#include <ad.h>
 
 uint8_t valor7Seg[] = {0, 0, 0, 0};
 uint16_t retardo;
 uint8_t cual;
 
 
-void refresh();
+void refresh(); ///función utilizada por el timer, para el refresco de la pantalla de siete segmentos
+void myUserFunction();
 void sieteSeg_valor (uint16_t valor);
 void sieteSeg_digitos (uint8_t * valor);
 void sieteSeg_init ();
 
+void myUserFunction(uint16_t * valores)
+{
+	
+			sieteSeg_valor(valores[0]);  ///envío el valor obtenido solo si ha cambiado
+}
 
 void sieteSeg_valor (uint16_t valor)
 {
@@ -54,8 +61,8 @@ void sieteSeg_init ()
 {
 	gpio_set_output_all_reg(SET_PIN_G); ///configuramos puerto G con todos sus pines en modo salida
 	sieteSeg_valor(0);
-	timer_init(5);
-	timer_repeat_call(1000,&refresh);
+	timer_init(5); ///factor de escala respecto al reloj principal
+	timer_repeat_call(20000,&refresh); ///valor en microsegundos de llamada
 	cual = 0;
 }
 void refresh()
@@ -67,6 +74,7 @@ void refresh()
 
 }
 
+
 int main ()
 {
 	uint16_t number=0;
@@ -76,6 +84,22 @@ int main ()
 	
 	sieteSeg_init();
 	
+	/*    ad_pin_inicio(3,0); ///utilizaremos el pin 3 para la recogida de valores
+
+		ad_modulo(0,0); ///módulo cero
+
+		ad_ocho_o_diez_bits(1,0); ///resolución de 10 bits
+
+        ad_ciclos_muestreo(3,0); ///16 ciclos por muestreo
+
+		ad_scanMode(1,0); ///activo modo scan
+
+		ad_cantidadConversiones(1,0); ///una conversión
+	
+		ad_activarInterrupMode(1); ///habilito el modo interrupción
+		ad_setUserFunction(&myUserFunction); ///configuro una rutina de usuario.
+		ad_start(); ///se inicia el proceso*/
+
 	while (1)
 	{
 		
